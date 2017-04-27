e28e6e095c7f60b9d223b9a4b4b47666a61c9db7
Scott BonAmi <scott@rootsrated.com>
2017-04-26 23:44:47 -0400

    Add coverage groups for proper identification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "stackadapt"
 
 require "simplecov"
-SimpleCov.start
+SimpleCov.start do
+  add_filter 'lib/stackadapt/core_ext'
+
+  add_group 'Models', 'lib/stackadapt/models'
+  add_group 'Client', 'lib/stackadapt/client'
+end
